3c987dd97ec7580683b8ee66fd1cf0eeb935aac7
DESKTOP-KT97LT3\LocNT <ngothanhloc90@gmail.com>
2022-09-09 07:09:47 +0700

    new commit

---
+++ 14-cart/src/data.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    title: "Samsung Galaxy S7",
+    price: 599.99,
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "google pixel ",
+    price: 499.99,
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 2",
+    price: 699.99,
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "iPhone 14",
+    price: 799.99,
+    img: "https://product.hstatic.net/200000348419/product/iphone_14_92e003de6e68438489218aad428c16f1.png",
+    amount: 1,
+  },
+];
